6f585491366e55ba58798c0165389b50a3ebf035
Jaturon Tepjuk <jaturon.tepjuk@gmail.com>
2022-01-01 19:32:55 +1100

    add the backend project

---
+++ src/main/java/com/NailShop/booking/model/Booking.java
@@ -0,0 +1,39 @@
+package com.NailShop.booking.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @SequenceGenerator(
+            name = "booking_sequence",
+            sequenceName = "booking_sequence"
+
+    )
+    @GeneratedValue(
+            generator = "booking_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long bookingId;
+
+    private String name;
+    private String lastName;
+    private String email;
+    private Integer phoneNumber;
+    private Date appointDate;
+
+
+
+}
